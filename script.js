0c83fc05211279bf1ac03c15602484e6b656171b
Rishav Dhiman <dhimanrishav2004@gmail.com>
2025-07-21 18:55:34 +0530

    Added Cursor Animations and Nav Bar

--- script.js
+++ script.js
@@ -1,40 +1,64 @@
-var tl = gsap.timeline();
-tl.from(".line h1, .line h2", {
-    y: 150,
-    stagger: 0.25,
-    duration: 0.6,
-    delay: 0.5,
-});
+function loadingAnimation() {
+    var tl = gsap.timeline();
+    tl.from(".line h1, .line h2", {
+        y: 150,
+        stagger: 0.25,
+        duration: 0.6,
+        delay: 0.5,
+    });
+    
+    tl.from("#line1-part1, line h2", {
+        opacity: 0,
+        onStart: function(){
+            var h5Timer = document.querySelector("#line1-part1 h5");
+            var grow = 0;
+            setInterval(function(){
+                if (grow<100){
+                    h5Timer.innerHTML = grow++;
+                } else {
+                    h5Timer.innerHTML = grow;
+                }
+            }, 20);
+        }
+    });
+    
+    tl.to("#loader", {
+        opacity: 0,
+        duration: 0.4,
+        delay: 2
+    });
+    
+    tl.from("#page1", {
+        delay: 0.2,
+        y: 1600,
+        opacity: 0,
+        duration: 0.5,
+        ease: Power4,
+    })
+    
+    tl.to("#loader", {
+        display: "none",
+    })
+    
+    tl.from("#nav", {
+        opacity: 0,
+    })
 
-tl.from("#line1-part1, line h2", {
-    opacity: 0,
-    onStart: function(){
-        var h5Timer = document.querySelector("#line1-part1 h5");
-        var grow = 0;
-        setInterval(function(){
-            if (grow<100){
-                h5Timer.innerHTML = grow++;
-            } else {
-                h5Timer.innerHTML = grow;
-            }
-        }, 30);
-    }
-});
+    tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero3 h3, #hero4 h1", {
+        y: 120,
+        stagger: 0.2,
+    })
+}
+loadingAnimation();
 
-tl.to("#loader", {
-    opacity: 0,
-    duration: 0.4,
-    delay: 3.1
-});
-
-tl.from("#page1", {
-    delay: 0.2,
-    y: 1600,
-    opacity: 0,
-    duration: 0.5,
-    ease: Power4,
-})
-
-tl.to("#loader", {
-    display: "none",
-})
+function cursorAnimation() {
+    document.addEventListener("mousemove", function(dets){
+        gsap.to("#crsr", {
+            left: dets.x,
+            top: dets.y,
+        });
+    }) 
+    
+    Shery.makeMagnet("#nav-part2 h4");
+}
+cursorAnimation();
